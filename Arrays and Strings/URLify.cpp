ad4afaa3ce5b6ea25b3daec00fda20cb06dfc9b5
Kannu Priya <44132532+Kannupriyasingh@users.noreply.github.com>
2022-11-07 14:40:41 +0530

    Create URLify.cpp

---
+++ Arrays and Strings/URLify.cpp
@@ -0,0 +1,36 @@
+/**********************************************************************************/
+Input : Mr John    Smith    "
+Output : Mr%20John%20Smith
+/**********************************************************************************/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+string solve (string s)
+{
+  int end = s.size (), i = 0;
+  string res = "";
+  while (s[end] == ' ')
+    end--;
+
+  while (i < end)
+    {
+      string curr = "";
+      while (s[i] != ' ')
+	    curr += s[i++];
+
+      res += (curr + "%20");
+      while (s[i] == ' ')
+	    i++;
+
+    }
+  return res.substr (0, res.size () - 3);
+}
+
+int main ()
+{
+  string s = "Mr John    Smith    ";
+  cout << solve (s);
+}
+
+
