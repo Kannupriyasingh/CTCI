081a98ebef0f4c044b158ed2fd93c75c4c3dac75
Kannu Priya <44132532+Kannupriyasingh@users.noreply.github.com>
2022-11-09 15:07:58 +0530

    Update 9q_String Rotation.cpp

--- Arrays and Strings/9q_String Rotation.cpp
+++ Arrays and Strings/9q_String Rotation.cpp
@@ -1,4 +1,38 @@
 /********************************************************************************************************************************************
 Leetcode Link of this Problem : https://leetcode.com/problems/rotate-string/
 
+Input: s = "abcde", goal = "cdeab"
+Output: true
+
+Input: s = "abcde", goal = "abced"
+Output: false
+
 *********************************************************************************************************************************************/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void rotate(string &s){
+    int i = 0;
+    char first_char = s[0];
+    while (i < s.size()-1){
+        s[i] = s[i+1];
+        i++;
+    }
+  s[s.size()-1] = first_char; 
+}
+bool string_rotation(string &s, string goal){
+    for(int i = 1; i <= s.size(); i++){
+        rotate(s);
+        if(s == goal) return true;
+    }
+    return false;
+}
+ 
+int main(){
+    string s = "rotation", goal = "rnotatio";
+    cout << string_rotation(s, goal);
+    return 0;
+}
+
